b47bc7c07c61ccae077cf5c0bdbd762022ca06c4
rubenmoya <rmoyarodriguez@gmail.com>
2017-11-26 21:12:43 +0100

    Fix things

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const SLACK_BOT_URL = 'https://hooks.slack.com/services/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';
 
@@ -16,7 +17,7 @@ app.post('/jobs', function (req, res) {
   fetch('https://nsjobs-f8648.firebaseio.com/jobs.json', { method: 'POST', body: `{"url" : "${req.body.text}"}` })
     .then(res => res.json())
     .then(json => fetch(SLACK_BOT_URL, { method: 'POST', body: offer }))
-    .then(() => res.send(201).send(offer))
+    .then(() => res.status(201).send(offer))
     .catch(err => {
       console.log(err);
       res.send(500);
