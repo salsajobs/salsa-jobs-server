920080cc1c2c7a37c4978f3c8da0cb147f8dc10d
rubenmoya <rmoyarodriguez@gmail.com>
2017-11-26 21:06:44 +0100

    Add slack bot request

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var bodyParser = require('body-parser');
-
+const app = require('express')();
+const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const firebase = require('firebase');
 const PORT = process.env.PORT || 3000;
-const express = require('express');
-const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', function (req, res) {
-    console.log(req.body.text);
-    fetch('https://nsjobs-f8648.firebaseio.com/jobs.json', { method: 'POST', body: `{"url" : "${req.body.text}"}` })
-        .then(res => res.json())
-        .then(json => {
-            res.send(json);
-        })
-        .catch(err => {
-            console.log(err);
-            res.send(500);
-        });
+const SLACK_BOT_URL = 'https://hooks.slack.com/services/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';
 
+app.post('/jobs', function (req, res) {
+  console.log(req.body.text);
+  const offer = {
+    text: req.body.text
+  };
+  
+  fetch('https://nsjobs-f8648.firebaseio.com/jobs.json', { method: 'POST', body: `{"url" : "${req.body.text}"}` })
+    .then(res => res.json())
+    .then(json => fetch(SLACK_BOT_URL, { method: 'POST', body: body }))
+    .then(() => res.send(201).send(offer))
+    .catch(err => {
+      console.log(err);
+      res.send(500);
+    });
 });
 
 app.listen(PORT);
