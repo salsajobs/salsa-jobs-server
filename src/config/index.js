bc2f5433cb5d40017cacb1003c20adb342c66a48
elenatorro <elenatorro@gmail.com>
2017-11-30 22:09:39 +0100

    Fix link

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const FIREBASE_SERVER = 'https://salsadev-8191f.firebaseio.com/';
+const FIREBASE_SERVER = 'https://salsadev-8191f.firebaseio.com/jobs.json';
 const SLACK_BOT_URL = 'https://hooks.slack.com/services/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';
 
 module.exports = { FIREBASE_SERVER, SLACK_BOT_URL };
