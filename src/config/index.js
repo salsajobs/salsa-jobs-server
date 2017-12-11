0af4f17086ef165e5d4532a018b549cd8bd20d37
IagoLast <iago.lastra@gmail.com>
2017-12-11 19:12:57 +0100

    change database

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const JOBS_DATABASE = 'jobs';
-const FIREBASE_URL = 'https://salsadev-8191f.firebaseio.com';
-// const FIREBASE_URL = 'https://nsjobs-f8648.firebaseio.com'
+// const FIREBASE_URL = 'https://salsadev-8191f.firebaseio.com';
+const FIREBASE_URL = 'https://nsjobs-f8648.firebaseio.com'
 const SLACK_BOT_URL = 'https://hooks.slack.com/services/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';
 
 module.exports = { FIREBASE_URL, SLACK_BOT_URL, JOBS_DATABASE };
