034bc572c103fd65c39479245e8a11217368a350
IagoLast <iago.lastra@gmail.com>
2017-12-11 20:49:26 +0100

    Handle attachements

--- src/jobs/Offer.js
+++ src/jobs/Offer.js
@@ -4,22 +4,26 @@ const URL_REGEX = /(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp
  *
  */
 
- class Offer {
+class Offer {
     /**
      *
      * @param {object} query
      */
     constructor(query) {
         console.log('\nOFFER: ', query);
-        this.link = this._getLink(query.text);
+        if (query.attachments) {
+            this.link = this._getLink(query.attachments[0].fallback);
+        } else {
+            this.link = this._getLink(query.text);
+        }
         this.description = this._getDescription(query.text, this.link);
         this.createdAt = Date.now();
         this.text = query.text;
         this.meta = query;
         this.shared = 0;
         this.votes = {
-          upvotes: [],
-          downvotes: []
+            upvotes: [],
+            downvotes: []
         };
     }
 
@@ -33,7 +37,7 @@ const URL_REGEX = /(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp
     }
 
     _getDescription(rawText, link) {
-      return rawText.replace(link, '').trim();
+        return rawText.replace(link, '').trim();
     }
 }
 
