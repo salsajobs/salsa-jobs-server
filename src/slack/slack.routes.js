6050a85642468223bf8be65f091c205264bdc5c3
IagoLast <iago.lastra@gmail.com>
2017-12-29 21:03:50 +0100

    Test message

--- src/slack/slack.routes.js
+++ src/slack/slack.routes.js
@@ -35,7 +35,45 @@ async function sendMessage(req, res) {
     const action = payload.actions[0];
     const uid = `${payload.user.id}/${payload.team.id}`;
 
-    res.send(200).end();
+    res.status(200).send({
+      "text": "Would you like to play a game?",
+      "attachments": [
+        {
+          "text": "Choose a game to play",
+          "fallback": "You are unable to choose a game",
+          "callback_id": "wopr_game",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "game",
+              "text": "Chess",
+              "type": "button",
+              "value": "chess"
+            },
+            {
+              "name": "game",
+              "text": "Falken's Maze",
+              "type": "button",
+              "value": "maze"
+            },
+            {
+              "name": "game",
+              "text": "Thermonuclear War",
+              "style": "danger",
+              "type": "button",
+              "value": "war",
+              "confirm": {
+                "title": "Are you sure?",
+                "text": "Wouldn't you prefer a good game of chess?",
+                "ok_text": "Yes",
+                "dismiss_text": "No"
+              }
+            }
+          ]
+        }
+      ]
+    });
     return SLACK_ACTIONS[action.value](payload, uid);
   } catch (error) {
     Logger.error('Slack:routes:sendMessage', { error });
