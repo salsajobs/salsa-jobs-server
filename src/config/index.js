2cecdac86997133bf6615fb6262d03e715a6f6cc
elenatorro <elenatorro@gmail.com>
2018-01-27 12:57:19 +0100

    Update CORS policy

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const DATABASE_NAME = process.env.DATABASE_NAME;
 const FIREBASE_URL = process.env.FIREBASE_URL;
 const SLACK_BOT_URL = process.env.SLACK_BOT_URL;
-const ALLOWED_ORIGINS = ['http://localhost:8080', 'https://nsjobs-f8648.firebaseapp.com'];
+const ALLOWED_ORIGINS = [process.env.ALLOWED_CLIENT_URL];
+
 module.exports = { FIREBASE_URL, SLACK_BOT_URL, DATABASE_NAME, ALLOWED_ORIGINS };
