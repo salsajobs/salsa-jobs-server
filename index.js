eaa30ffba2534216d53c4bf1aeee8d0acdc29f94
IagoLast <iago.lastra@gmail.com>
2018-03-01 18:58:31 +0100

    Generate firebase token before start

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const config = require('./src/config/index');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = require('express')();
-const router = require('./src/jobs/jobs.router');
-const PORT = process.env.PORT || 3000;
+// Get firebase access on start
+require('./scripts/generateToken').generateToken().then(token => {
+  process.env.FIREBASE_CREDENTIALS = token;
+    
+  const config = require('./src/config/index');
+  const cors = require('cors');
+  const bodyParser = require('body-parser');
+  const app = require('express')();
+  const router = require('./src/jobs/jobs.router');
+  const PORT = process.env.PORT || 3000;
+    
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cors({ origin: config.ALLOWED_ORIGINS }));
+  app.post('/jobs', router.post);
+  app.post('/slack', router.vote);
+  app.get('/jobs', router.list);
+  
+  app.listen(PORT);
+});
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ origin: config.ALLOWED_ORIGINS }));
-app.post('/jobs', router.post);
-app.post('/slack', router.vote);
-app.get('/jobs', router.list);
-app.listen(PORT);
--- scripts/generateToken.js
+++ scripts/generateToken.js
@@ -1,16 +1,30 @@
-// DO NOT RUN DIRECTLY, USE FROM NPM SCRIPTS INSTEAD!
-
-// This script is a helper used to create firebase tokens in order to read/write from the database.
-// https://firebase.google.com/docs/auth/admin/create-custom-tokens?hl=es-419
-
-const uid = 'staging';
+const UID = 'staging';
 var admin = require('firebase-admin');
 
-var serviceAccount = require('./credentials.json');
+const serviceAccount = {
+  type: process.env.FRB_CRED_TYPE,
+  project_id: process.env.FRB_PROJECT_ID,
+  private_key_id: process.env.FRB_KEY_ID,
+  private_key: process.env.FRB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FRB_CLIENT_EMAIL,
+  client_id: process.env.FRB_CLIENT_ID,
+  auth_uri: process.env.FRB_AUTH_URI,
+  token_uri: process.env.FRB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FRB_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FRB_AUTH_PROVIDER_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_URL 
+  databaseURL: process.env.FIREBASE_URL
 });
 
-admin.auth().createCustomToken(uid).then(console.log).catch(console.error);
+
+function generateToken() {
+  return admin.auth().createCustomToken(UID);
+}
+
+
+module.exports = {
+  generateToken,
+};
